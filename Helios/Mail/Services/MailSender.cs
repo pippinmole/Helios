e2afbb6336441f84ea4f97e303800004d95bdfea
pippin_mole <40035529+pippinmole@users.noreply.github.com>
2022-02-16 23:30:19 +0000

    Removed OrderState from PaypalOrder

--- Helios/Mail/Services/MailSender.cs
+++ Helios/Mail/Services/MailSender.cs
@@ -1,9 +1,6 @@
-using System.Collections.Generic;
 using System.Net;
 using System.Net.Mail;
 using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 namespace Helios.MailService; 
--- Helios/Paypal/PaypalOrder.cs
+++ Helios/Paypal/PaypalOrder.cs
@@ -2,22 +2,15 @@
 
 namespace Helios.Paypal;
 
-public enum EOrderState {
-    Created,
-    Completed
-}
-
 public class PaypalOrder {
     
     [BsonId]
     public Guid Id { get; set; }
     public string OrderId { get; private set; }
     public int Sku { get; private set; }
-    public EOrderState State { get; set; }
 
     public PaypalOrder(int sku, string orderId) {
         Sku = sku;
         OrderId = orderId;
-        State = EOrderState.Created;
     }
 }
